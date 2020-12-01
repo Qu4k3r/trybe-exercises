90b963918f1a5f16bd2bad5d25f93ce1c6053ad2
Qu4k3r <neves0007@gmail.com>
2020-12-01 00:55:31 -0300

    Finish exercises of functions-intro.js | Except Bonus

--- Introduction-WebDevelopment/block-04-JavaScript-Intro/4.4/functions-intro.js
+++ Introduction-WebDevelopment/block-04-JavaScript-Intro/4.4/functions-intro.js
@@ -54,3 +54,75 @@ function indexOfLowestValue(list) {
 }
 
 console.log(indexOfLowestValue(myArray))
+
+// Exercise 4
+myArray = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']
+
+function greatestArrayElement(list) {
+  let greatestLengthElement = String()
+  for (let element of list) {
+    if (element.length > greatestLengthElement.length) {
+      greatestLengthElement = element
+    }
+  }
+  return greatestLengthElement
+}
+
+console.log(greatestArrayElement(myArray))
+
+// Exercise 5
+myArray = [2, 3, 2, 5, 8, 2, 3]
+
+function countingRepeatedElements(list) {
+  let numberOfRepetitions = []
+  let count = 0
+  for (let i = 0; i < list.length; i++) {
+    for (let j of list) {
+      if (j === list[i]) {
+        count += 1
+      }
+    }
+    numberOfRepetitions.push(count)
+    count = 0
+  }
+  let greatestElement = String()
+
+  // I called the function of Exercise 2 in order to spare time (please forgive me)
+  greatestElement = myArray[indexOfGreatestValue(numberOfRepetitions)]
+
+  return greatestElement
+}
+
+console.log(countingRepeatedElements(myArray))
+
+// Exercise 6
+function somatory (number) {
+  number = Number(number)
+  sum = 0
+  for (let i = 0; i <= number; i++) {
+    sum += i
+  }
+  return sum
+}
+
+console.log(somatory(5))
+
+// Exercise 7
+function verifiesWordsEnd(word, ending) {
+  word = String(word)
+  ending = String(ending)
+  let isEndingTrue = false
+  let wordIndex = 1
+  for (let lastLetter = ending.length - 1; lastLetter >= 0; lastLetter--) {
+    if (ending[lastLetter] !== word[word.length - wordIndex]) {
+      return isEndingTrue
+    }
+    else {
+      wordIndex++
+    }
+  }
+  isEndingTrue = true
+  return isEndingTrue
+}
+
+console.log(verifiesWordsEnd('joaofernando', 'fernano'))
