62f801b327f90e87f58b62d6c306ac9861455146
Qu4k3r <neves0007@gmail.com>
2020-12-08 01:13:52 -0300

    Block 05 day 4 | Update some features

--- Introduction-WebDevelopment/block-05-DOM&Selectors/5.4/script.js
+++ Introduction-WebDevelopment/block-05-DOM&Selectors/5.4/script.js
@@ -1,20 +1,23 @@
 // Step 1
 function changeBackgroundColor() {
+  document.body.style.background = localStorage.getItem('backgroundColor')
   const myButton = document.querySelector('#btn-bckg-color')
   const inputBckgColor = document.querySelector('input')
   myButton.addEventListener('click', () => {
     document.body.style.background = inputBckgColor.value
+    localStorage.setItem('backgroundColor', document.body.style.background)
   })
 }
-
 changeBackgroundColor()
 
 // Step 2
 function changeTextColor() {
+  document.body.style.color = localStorage.getItem('textColor')
   const myButton = document.querySelector('#btn-text-color')
   const inputTextColor = document.querySelector('#text-color')
   myButton.addEventListener('click', () => {
     document.body.style.color = inputTextColor.value
+    localStorage.setItem('textColor', document.body.style.color)
   })
 }
 
