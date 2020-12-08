4b2071d2adb37d1b791acb66ec5323c08ab86b5d
Qu4k3r <neves0007@gmail.com>
2020-12-08 00:04:15 -0300

    Block 05 day 4 | Step 2 & Update some features

--- Introduction-WebDevelopment/block-05-DOM&Selectors/5.4/script.js
+++ Introduction-WebDevelopment/block-05-DOM&Selectors/5.4/script.js
@@ -1,10 +1,21 @@
 // Step 1
 function changeBackgroundColor() {
   const myButton = document.querySelector('#btn-bckg-color')
-  const inputColor = document.querySelector('input')
+  const inputBckgColor = document.querySelector('input')
   myButton.addEventListener('click', () => {
-    document.body.style.background = inputColor.value
+    document.body.style.background = inputBckgColor.value
   })
 }
 
 changeBackgroundColor()
+
+// Step 2
+function changeTextColor() {
+  const myButton = document.querySelector('#btn-text-color')
+  const inputTextColor = document.querySelector('#text-color')
+  myButton.addEventListener('click', () => {
+    document.body.style.color = inputTextColor.value
+  })
+}
+
+changeTextColor()
