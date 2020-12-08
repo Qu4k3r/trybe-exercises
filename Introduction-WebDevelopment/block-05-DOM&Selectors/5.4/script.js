3b426882adcdc92b5a8e2c1edff7bee4ef2d2f7e
Qu4k3r <neves0007@gmail.com>
2020-12-08 04:05:37 -0300

    Block 05 day 4 | Try to fix some font size error | not good

--- Introduction-WebDevelopment/block-05-DOM&Selectors/5.4/script.js
+++ Introduction-WebDevelopment/block-05-DOM&Selectors/5.4/script.js
@@ -1,24 +1,51 @@
 // Step 1
-function changeBackgroundColor() {
-  document.body.style.background = localStorage.getItem('backgroundColor')
-  const myButton = document.querySelector('#btn-bckg-color')
-  const inputBckgColor = document.querySelector('input')
-  myButton.addEventListener('click', () => {
-    document.body.style.background = inputBckgColor.value
-    localStorage.setItem('backgroundColor', document.body.style.background)
-  })
-}
-changeBackgroundColor()
+window.onload = function () {
+  function changeBackgroundColor() {
+    document.body.style.background = localStorage.getItem('backgroundColor')
+    const myButton = document.querySelector('#btn-bckg-color')
+    const inputBckgColor = document.querySelector('input')
+    myButton.addEventListener('click', () => {
+      document.body.style.background = inputBckgColor.value
+      localStorage.setItem('backgroundColor', document.body.style.background)
+    })
+  }
+  changeBackgroundColor()
 
-// Step 2
-function changeTextColor() {
-  document.body.style.color = localStorage.getItem('textColor')
-  const myButton = document.querySelector('#btn-text-color')
-  const inputTextColor = document.querySelector('#text-color')
-  myButton.addEventListener('click', () => {
-    document.body.style.color = inputTextColor.value
-    localStorage.setItem('textColor', document.body.style.color)
-  })
-}
+  // Step 2
+  function changeTextColor() {
+    document.body.style.color = localStorage.getItem('textColor')
+    const myButton = document.querySelector('#btn-text-color')
+    const inputTextColor = document.querySelector('#text-color')
+    myButton.addEventListener('click', () => {
+      document.body.style.color = inputTextColor.value
+      localStorage.setItem('textColor', document.body.style.color)
+    })
+  }
 
-changeTextColor()
+  changeTextColor()
+
+  // Step 3
+    function modifyTextSize() {
+      if (localStorage['fontSize']) {
+        fontSize = Number(localStorage['fontSize'])
+      } else {
+        localStorage.setItem('fontSize', 16)
+        let fontSize = localStorage.getItem('fontSize')
+      }
+      const incr = 1
+      const myIncrButton = document.querySelector('#btn-incr-text-size')
+      const myDecrButton = document.querySelector('#btn-decr-text-size')
+      myIncrButton.addEventListener('click', () => {
+        fontSize += incr
+        document.body.style.fontSize = fontSize + 'px'
+        localStorage['fontSize'] = fontSize
+      })
+      myDecrButton.addEventListener('click', () => {
+        fontSize -= incr
+        document.body.style.fontSize = fontSize + 'px'
+        localStorage['fontSize'] = fontSize
+      })
+    }
+
+    modifyTextSize()
+  }
