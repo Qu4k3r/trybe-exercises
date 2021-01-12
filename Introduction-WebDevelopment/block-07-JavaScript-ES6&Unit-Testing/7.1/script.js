191bd7b7dc00ffde35dc96c3132de7547f6d44fc
Qu4k3r <neves0007@gmail.com>
2021-01-12 15:18:45 -0300

    Block 07 day1 & README.md

---
+++ Introduction-WebDevelopment/block-07-JavaScript-ES6&Unit-Testing/7.1/script.js
@@ -0,0 +1,14 @@
+// Exercise 1
+function testingScope(escopo) { 
+  if (escopo === true) { 
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+}
+
+testingScope(false);
