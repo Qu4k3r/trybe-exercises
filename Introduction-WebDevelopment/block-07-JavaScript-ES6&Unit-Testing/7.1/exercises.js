225edd7cccd2d43ee579a8951971b76c5b530507
Qu4k3r <neves0007@gmail.com>
2021-01-12 18:27:59 -0300

    Exercise 2 | Part 3

---
+++ Introduction-WebDevelopment/block-07-JavaScript-ES6&Unit-Testing/7.1/exercises.js
@@ -0,0 +1,51 @@
+// Exercise 1
+function testingScope(escopo) { 
+  if (escopo === true) { 
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+}
+
+testingScope(false);
+
+//Exercise 2 part 1
+const factorial = num => {
+  for (let previousNumbers = num - 1; previousNumbers > 0; previousNumbers -= 1) {
+    num *= previousNumbers
+  }
+  return num
+}
+
+console.log(factorial(6))
+
+//Recursive way (BEAUTIFUL)
+const factorialNumber = num => {
+  if (num <= 1) {
+    return 1
+  }
+  else {
+    return num * factorialNumber(num - 1)
+  }
+}
+
+console.log(factorialNumber(3))
+
+//Exercise 2 part 2
+const splitPhrase = phrase => phrase.split(' ')
+
+const getGreatestWord = (phrase) => {
+  let greater = '';
+  splitPhrase(phrase).forEach(word => {
+    if(word.length >= greater.length) {
+      greater = word
+    }
+  })
+  return greater
+}
+
+console.log(getGreatestWord('oh mama mia mama mia ... mama mia letmego'))
--- Introduction-WebDevelopment/block-07-JavaScript-ES6&Unit-Testing/7.1/script.js
+++ Introduction-WebDevelopment/block-07-JavaScript-ES6&Unit-Testing/7.1/script.js
@@ -1,52 +1,7 @@
-// Exercise 1
-function testingScope(escopo) { 
-  if (escopo === true) { 
-    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-    console.log(ifScope);
-  } else {
-    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
-  }
-  // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+const clickCount = () => {
+  let count = 0
+  const myButton = document.querySelector('button')
+  myButton.addEventListener('click', (event) => event.target.innerText = `I've been clicked ${count+=1} times!!`)
 }
 
-testingScope(false);
-
-//Exercise 2 part 1
-const factorial = num => {
-  for (let previousNumbers = num - 1; previousNumbers > 0; previousNumbers -= 1) {
-    num *= previousNumbers
-  }
-  return num
-}
-
-console.log(factorial(6))
-
-//Recursive way (BEAUTIFUL)
-const factorialNumber = num => {
-  if (num <= 1) {
-    return 1
-  }
-  else {
-    return num * factorialNumber(num - 1)
-  }
-}
-
-console.log(factorialNumber(3))
-
-//Exercise 2 part 2
-const splitPhrase = phrase => phrase.split(' ')
-
-const getGreatestWord = (phrase) => {
-  let greater = '';
-  splitPhrase(phrase).forEach(word => {
-    if(word.length >= greater.length) {
-      greater = word
-    }
-  })
-  return greater
-}
-
-console.log(getGreatestWord('oh mama mia mama mia ... mama mia letmego'))
-// console.log('ola, tudo bem?'.split(' '))
+clickCount()
