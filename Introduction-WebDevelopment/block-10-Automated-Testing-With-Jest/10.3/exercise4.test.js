812cabe9742c4ac030eed1deb54230e48306e338
Qu4k3r <neves0007@gmail.com>
2021-02-09 17:39:05 -0300

    Exercise 4 modified but unfinished | I'm commiting in order to adjust the main README

--- Introduction-WebDevelopment/block-10-Automated-Testing-With-Jest/10.3/exercise4.test.js
+++ Introduction-WebDevelopment/block-10-Automated-Testing-With-Jest/10.3/exercise4.test.js
@@ -1,29 +1,60 @@
 let fetch = require('node-fetch');
 
 let fetchDogPictures = async () => {
-  const response = await fetch('https://dog.ceo/api/breeds/image/random');
-  const json = await response.json();
+  
 
   // json.status === 'success' ? 'Request success' : 'Request failed';
 
-  response.ok ? Promise.resolve(json) : Promise.reject(json);
+  // response.ok ? Promise.resolve(json) : Promise.reject(json);
+  // console.log(json)
+  try {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    const json = await response.json();
+    return json.message;
+  } catch(e) {
+    return e;
+  }
 
 };
 
 describe('Testa as possiveis respostas da API', () => {
 
-  fetchDogPictures = jest.fn();
+  // jest.mock('node-fetch');
+  fetch = jest.fn();
+  
+  // fetchDogPictures = jest.fn();
+
+  // afterEach(() => fetch.mockRestore());
 
   it('Testa se a resposta foi resolvida', () => {
-    fetchDogPictures.mockResolvedValue('Request success');
 
-    return expect(fetchDogPictures()).resolves.toEqual('Request success');
+    const mockedJson = {
+      message: 'Request success',
+    }
+
+    fetch.mockImplementation(() => Promise.resolve({ json: () => Promise.resolve(mockedJson) }));
+    // fetchDogPictures.mockResolvedValue('Request success');
+
+    return expect(fetchDogPictures()).resolves.toBe('Request success');
   });
 
   it('Testa se a resposta foi rejeitada', () => {
-    fetchDogPictures.mockRejectedValue('Request failed');
 
-    return expect(fetchDogPictures()).rejects.toEqual('Request failed');
+    const mockedJson = {
+      message: 'Request failed',
+    }
+
+    fetch.mockImplementation(() => Promise.reject('erro'));
+    // fetchDogPictures.mockRejectedValue('Request failed');
+
+    return expect(fetchDogPictures()).rejects.toBe('erro');
 
   });
 });
+
+
+// {
+//   status: 'error',
+//   message: 'No route found for "GET /api/breeds/image/randomm" with code: 0',
+//   code: 404
+// }
